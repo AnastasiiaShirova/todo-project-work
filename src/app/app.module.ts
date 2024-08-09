7a03e00e85dd6ce4f2cc85f1b56692233d6b4b55
AnastasiiaShirova <anastasiyamironova711@gmail.com>
2024-08-09 13:13:17 +0300

    build: todo-library
    create a todo-library, move todo-module from todo-project to todo-library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoModule } from './modules/todo/todo.module';
+import { TodoModule } from 'todo-library';
 
 @NgModule({
   declarations: [AppComponent],
